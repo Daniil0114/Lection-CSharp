22f10c23dadc7678d6a9be0b37ac6941577e5604
Daniil <danich.sh.cher01@gmail.com>
2023-07-10 00:42:23 +0300

    Создали Example008

---
+++ Example008_Summa_4/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random(). Next (1, 15); //1, 2, 3, ...14
+Console.WriteLine(numberA);
+int numberB = new Random(). Next (1, 20);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
