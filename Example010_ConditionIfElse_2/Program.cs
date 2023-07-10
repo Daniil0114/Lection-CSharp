1db0b94e382ade5c122ed9c75cb76e6aa4a630cf
Daniil <danich.sh.cher01@gmail.com>
2023-07-10 19:41:55 +0300

    Создали Example010

---
+++ Example010_ConditionIfElse_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "daniel") //.ToLower- позволяет все символы перевести в  нижний регистр
+{
+    Console.WriteLine("Hooray, it's Daniel");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
