26e37f0945e2208226edd309b961a0c883ec2a82
Daniil <danich.sh.cher01@gmail.com>
2023-07-10 19:32:29 +0300

    Создали Example009

---
+++ Example009_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+if(username == "Daniel")
+{
+    Console.WriteLine("Hooray, it's Daniel");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
